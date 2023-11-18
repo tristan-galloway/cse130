3c0cfb43b8c53fd66f24e9efd58b53b1c6d25b72
tristan-galloway <tristan.a.galloway@gmail.com>
2023-11-17 22:51:29 -0700

    Finished W10 Lab

--- week-10-loops/discussion-board.py
+++ week-10-loops/discussion-board.py
@@ -1,6 +1,17 @@
+# %%
 user_number = int(input("Number: "))
 user_range = range(1, user_number + 1)
 
 for number in user_range:
     sq_number = 2 ** number
     print(sq_number)
+
+
+# %%
+acc_balances = [1500, 20000, 15000, -200000, -2000, -25000]
+net_worth = 0
+
+for balance in acc_balances:
+    net_worth += balance
+
+# %%
--- week-10-loops/lab10-numerical-sequence.py
+++ week-10-loops/lab10-numerical-sequence.py
@@ -6,22 +6,44 @@
 #      This program takes a user given number, and computes what that number would be
 #      In francois counting system.
 # 4. What was the hardest part? Be as specific as possible.
-#      -a paragraph or two about how the assignment went for you-
+#      I think for me it's still the asserts. I went back and watched the supplemental
+#      videos on asserts where you explained that they aren't supposed to catch user 
+#      errors but instead catch errors of future coding changes to the function I think
+#      helped me understand the way we are supposed to be using them.
 # 5. How long did it take for you to complete the assignment?
-#      -total time in hours including reading the assignment and submitting the program-
-
-import time
+#      It took about 3 hours. I'm not totally sure since it was very split up because I
+#      am taking care of my fiance. She just got her tonsils removed, so my work flow was 
+#      very scattered over the last few days.
 
 def main():
-    """Runs the following test cases and prints the results as it goes.
-    (-1, 0, 1, 2, 9, 100, 200)
-    """
-    
-    user_number = ""
 
-    while user_number != "done":
-        user_number = input("Enter a number (type done when finished): ")
-        print(f_number(int(user_number)))
+    # Set the user_number variable so it will run the first time.
+    user_number = 0
 
-def f_number(user_number):
-    pass
+    # Continue asking for a number while the given number is a non positive integer.
+    while user_number <= 0:
+        user_number = int(input("Enter a number: "))
+
+    # Initialize the numbers list to house two locations to store values.
+    numbers = [1, 2] #[1%2]=[1] and [2%2]=[0]
+    assert numbers == [1, 2]
+
+    # Alternate the location to combine the numbers in slot one and two
+    # in the list numbers.
+    if user_number > 2:
+        assert user_number > 2
+        for i in range(3, user_number + 1):
+            numbers[i % 2] = numbers[0] + numbers[1]
+
+    # Set the value as the higher of the two that was last added to.
+    value = numbers[user_number % 2]
+
+    # Value should be a positive number
+    assert value >= 0
+
+    # Print the users number in francois counting system.
+    return print(f"\nFracois number is {value}")
+
+# If the program is imported, ignore the call to main.
+if __name__ == "__main__":
+    main()
